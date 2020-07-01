018936b777292fef6a10ccd720697605be7b472f
Andrey Novik <bignovik@gmail.com>
2020-07-01 19:23:07 +0300

    Text on image

--- src/components/artist-lot/LotCard.tsx
+++ src/components/artist-lot/LotCard.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import FsLightbox from 'fslightbox-react'
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, withTheme } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
+import Grid from '@material-ui/core/Grid'
 import CardActionArea from '@material-ui/core/CardActionArea'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
@@ -13,6 +14,29 @@ const useStyles = makeStyles({
   },
   media: {
     height: 280,
+  },
+  imageContainer: {
+    position: 'relative',
+  },
+  image: {
+    width: '100%',
+  },
+  imageContent: {
+    position: 'absolute',
+    padding: '10px 15px',
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0,
+    color: 'white',
+    backgroundColor: '#00000000',
+    opacity: 0.2,
+    transition: '.3s ease-in-out',
+
+    '&:hover': {
+      opacity: 1,
+      backgroundColor: '#0000006a',
+    },
   },
 })
 
@@ -42,8 +66,34 @@ const LotCard: React.FC<unknown> = () => {
       </CardActionArea>
       <FsLightbox
         toggler={toggler}
-        sources={[
-          'https://uploads6.wikiart.org/images/pablo-picasso/portrait-of-suzanne-bloch-1904.jpg',
+        customSources={[
+          <div
+            className={classes.imageContainer}
+            key="https://uploads6.wikiart.org/images/pablo-picasso/portrait-of-suzanne-bloch-1904.jpg"
+          >
+            <img
+              className={classes.image}
+              src="https://uploads6.wikiart.org/images/pablo-picasso/portrait-of-suzanne-bloch-1904.jpg"
+              alt="Snow"
+            />
+            <Grid
+              container
+              direction="column"
+              alignItems="flex-end"
+              justify="flex-end"
+              className={classes.imageContent}
+            >
+              <Typography gutterBottom variant="body1" component="h4">
+                Portrait of Suzanne Bloch, 1904
+              </Typography>
+              <Typography variant="body2" component="p">
+                Painting
+              </Typography>
+              <Typography variant="body2" component="p">
+                $ 150,000
+              </Typography>
+            </Grid>
+          </div>,
         ]}
       />
     </Card>
