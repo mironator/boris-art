cf2645737e74862b7df044ca957392b90ba44d02
Dmitry Mironchenko <dmironchenko@What-are-you-looking-at.local>
2020-08-03 16:04:50 +0300

    Fix API changes regression

--- pages/api/artists/medium-list/[id].ts
+++ pages/api/artists/medium-list/[id].ts
@@ -12,7 +12,7 @@ const handler: (_req: NextApiRequest, res: NextApiResponse) => Promise<void> = a
       `http://54.156.225.113:8000/v1/artist-medium-list?artist_id[eq]=${id}`
     )
     const data = await apiRes.json()
-    const mediumList = _.get(data, 'payload.artist-medium-list')?.map(
+    const mediumList = _.get(data, 'payload.artist_medium_list')?.map(
       (item: any) => item.medium
     ) as string[]
 
