c4a99c8f5f579c335275ccc8f557f35d66b1b19c
Dmitry Mironchenko <dmironchenko@What-are-you-looking-at.local>
2020-08-06 15:56:55 +0300

    FIx server uri

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,7 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 export const HTTP_SERVER = dev ? 'http://localhost:3000' : 'https://boris-art.vercel.app'
-export const SERVER = dev
-  ? 'http://localhost:3000/api/graphql'
-  : 'https://boris-art.vercel.app/api/graphql'
+export const SERVER = dev ? '/api/graphql' : '/api/graphql'
 export const WEB_SOCKET_LINK = dev
   ? 'ws://localhost:3000/api/graphql'
   : 'https://boris-art.vercel.app/api/graphql'
