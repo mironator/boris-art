8667915f4d906cd38c4d7918a6accc0c8e82403e
Andrey Novik <bignovik@gmail.com>
2020-09-01 13:48:19 +0300

    AppBar

--- src/components/artist-analytics/ArtistAnalytics.tsx
+++ src/components/artist-analytics/ArtistAnalytics.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Grid, Typography } from '@material-ui/core'
+import { AppBar, Grid, Tab, Tabs, Typography } from '@material-ui/core'
+import classNames from 'classnames'
 
 import { Artist } from '@interfaces/index'
 import mediumTypes from '@hooks/mediumTypes'
@@ -10,6 +11,14 @@ import ArtworkIndexCompareChart from './charts/ArtworkIndexCompareChart'
 import ReturnsVSHoldingPeriodChart from './charts/ReturnsVSHoldingPeriodChart'
 import CompoundAnnualReturns from './charts/CompoundAnnualReturns'
 
+import useStyles from './ArtistAnalytics.styles'
+
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: any
+  value: any
+}
+
 interface OwnProps {
   artistInfo: Artist
   mediumList: Array<keyof typeof mediumTypes>
@@ -17,7 +26,33 @@ interface OwnProps {
 
 type Props = OwnProps
 
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props
+  const classes = useStyles()
+
+  return (
+    <Grid
+      item
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      className={classNames({
+        [classes.hidden]: value !== index,
+      })}
+      {...other}
+    >
+      {children}
+    </Grid>
+  )
+}
+
 const ArtistAnalytics: React.FC<Props> = ({ artistInfo, mediumList }) => {
+  const [barValue, setBarValue] = React.useState(0)
+
+  const handleBarChange = (_event: React.ChangeEvent<{}>, newValue: number) => {
+    setBarValue(newValue)
+  }
+
   if (!artistInfo.id) {
     return null
   }
@@ -25,30 +60,17 @@ const ArtistAnalytics: React.FC<Props> = ({ artistInfo, mediumList }) => {
   return (
     <Grid container direction="column" spacing={5}>
       <Grid item>
-        <Typography variant="h5" component="h2">
-          Artwork Index
-        </Typography>
+        <Tabs value={barValue} onChange={handleBarChange} aria-label="Artist analytics">
+          <Tab label="Artwork Index" />
+          <Tab label="Artist Comparison" />
+        </Tabs>
       </Grid>
-      <Grid item>
+      <TabPanel value={barValue} index={0}>
         <ArtworkIndexAllCharts artistId={artistInfo.id} mediumList={mediumList} />
-      </Grid>
-      <Grid item>
-        <Typography variant="h5" component="h2">
-          Artist Comparison
-        </Typography>
-      </Grid>
-      <Grid item>
+      </TabPanel>
+      <TabPanel value={barValue} index={1}>
         <ArtworkIndexCompareChart artist={artistInfo} />
-      </Grid>
-
-      {/* <Grid item>
-        <Typography variant="h5" component="h2">
-          Artwork Index
-        </Typography>
-      </Grid>
-      <Grid item>
-        <ArtworkIndexChart artistId={artistInfo.id} />
-      </Grid> */}
+      </TabPanel>
       <Grid item>
         <Typography variant="h5" component="h2">
           Annual Returns vs Holding Period
